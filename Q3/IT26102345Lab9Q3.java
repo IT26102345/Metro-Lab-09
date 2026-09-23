75ee7626616b15b909105d70ea8bd4ca74fbc24c
IT26102345 <it26102345@my.sliit.lk>
2026-09-23 20:34:14 +0530

    Lab9Q3

---
+++ Q3/IT26102345Lab9Q3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class IT26102345Lab9Q3 {
+
+    public static int add (int a, int b) {
+	    return a + b;
+	}
+	
+	public static int multiply (int a, int b) {
+	    return a * b;
+	}
+	
+	public static int square (int a) {
+	    return a * a;
+	}
+	
+	public static void main (String [] args) {
+	
+	    int result1 = square (add (multiply (3, 4), multiply (5, 7)));
+		
+		int result2 = add (square (add (4, 7)), square (add (8, 3)));
+		
+		System.out.println ();
+		System.out.println ("Result of (3 * 4 + 5 * 7)^2    : " + result1);
+		System.out.println ("Result of (4 + 7)^2 + (8 + 3)^2 : " + result2);
+	}
+}
+		
