f4c2c98946c5597ee32a5663d94ac8ec1b7fc172
IT26102345 <it26102345@my.sliit.lk>
2026-09-23 20:33:41 +0530

    Lab9Q2

---
+++ Q2/IT26102345Lab9Q2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IT26102345Lab9Q2 {
+
+    public static double circleArea (double radius) {
+	
+	    double area = Math.PI * radius * radius;
+		return area;
+	}
+	
+	public static void main (String [] args ) {
+	
+	    Scanner input = new Scanner (System.in);
+		
+		System.out.print ("Enter the radius of the circle : ");
+		double radius = input.nextDouble ();
+		
+		double area = circleArea (radius);
+		
+		System.out.println ("The area of the circle with radius " + radius + "is : " + area);
+	}
+}
